7806677df6d39b1e7293d58a84e25e9c9cf92f49
nilsedgar <nils.jacobsen@zocom.se>
2025-05-12 12:44:30 +0200

    Initial commit

---
+++ src/main/java/com/example/springwithgit/SpringwithgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwithgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwithgit/SpringwithgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwithgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
